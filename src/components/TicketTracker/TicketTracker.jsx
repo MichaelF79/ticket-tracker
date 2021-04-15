0fee7da044fab2367002caf9af83b879ebe3fddd
Michael Ferreira <ferreira.mjm@gmail.com>
2021-04-15 12:13:23 +0100

    Working tracker

--- src/components/TicketTracker/TicketTracker.jsx
+++ src/components/TicketTracker/TicketTracker.jsx
@@ -5,24 +5,27 @@ import TicketCounter from '../TicketCounter';
 import styles from './TicketTracker.module.scss';
 
 const TicketTracker = ({ employee }) => {
-  const [ticketsCompleted, setTicketsCompleted] = useState(0);
+  const [Completed, setCompleted] = useState(0);
+
   const { name, role } = employee;
+
   const increment = () => {
-    setTicketsCompleted(ticketsCompleted + 1);
+    setCompleted(Completed + 1);
   };
-  const limitedDecrement = () => {
-    if (ticketsCompleted > 0) {
-      setTicketsCompleted(ticketsCompleted - 1);
+  const decrement = () => {
+    if (Completed > 0) {
+      setCompleted(Completed - 1);
     }
   };
+
   return (
-    <article className={styles.Tracker}>
+    <article className={styles.TicketTracker}>
       <h2 className={styles.name}>{name}</h2>
       <h3 className={styles.role}>{role}</h3>
       <TicketCounter
-        tickets={ticketsCompleted}
+        tickets={Completed}
         increment={increment}
-        decrement={limitedDecrement}
+        decrement={decrement}
       />
     </article>
   );
