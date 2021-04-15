720ae64dfe8d36f1a2a65e9646821a28fdf71fd2
Michael Ferreira <ferreira.mjm@gmail.com>
2021-04-15 13:21:35 +0100

    Finished without styling

--- src/components/TicketCounter/TicketCounter.jsx
+++ src/components/TicketCounter/TicketCounter.jsx
@@ -5,9 +5,7 @@ const TicketCounter = ({ tickets, decrement, increment }) => {
     <section>
       <h4>Completed:</h4>
       <p>{tickets}</p>
-      <button disabled={tickets <= 0} onClick={decrement}>
-        -
-      </button>
+      <button onClick={decrement}>-</button>
       <button onClick={increment}>+</button>
     </section>
   );
--- src/data/teams.js
+++ src/data/teams.js
@@ -7,6 +7,7 @@ const team = [
   {id: 6,name: "Karam Lees",role: "Senior Software Developer"},
   {id: 7,name: "Eleanor Dowling",role: "Project Manager"},
   {id: 8,name: "Haris Grey",role: "Tester"},
-  {id: 9,name: "Sohaib Farley",role: "Tester"},{id: 10,name: "Phillip Mitchell",role: "Hard Man"},
+  {id: 9,name: "Sohaib Farley",role: "Tester"},
+  {id: 10,name: "Phillip Mitchell",role: "Hard Man"},
 ]
 export default team;
